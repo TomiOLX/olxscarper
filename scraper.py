c51b6bb4d797f8de70a240f060b9f905acbbc89d
TomiOLX <tdesperak1@gmail.com>
2025-08-11 23:49:44 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -84,3 +84,8 @@ def main():
 
 if __name__ == "__main__":
     main()
+    
+    headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+}
+resp = requests.get(OLX_URL, headers=headers)
